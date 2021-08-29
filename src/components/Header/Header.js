263714a8f023835979eecbfddf42a997cbe1f12e
Slawek <61154596+Ultran@users.noreply.github.com>
2021-08-29 23:24:48 +0200

    add elements to header, to do: add logo and reasponsivness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,30 @@
-const Header = () => {
-  return <p>header</p>;
-};
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import ShoppingCartRoundedIcon from "@material-ui/icons/ShoppingCartRounded";
+import StarRoundedIcon from "@material-ui/icons/StarRounded";
 
+import "./sass/app.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <div className="logo-img">LOGO</div>
+      </div>
+      <div className="text">
+        <h4>welcome in wall-e</h4>
+      </div>
+      <div className="buttons">
+        <div className="icon">
+          <AccountCircleRoundedIcon />
+        </div>
+        <div className="icon">
+          <ShoppingCartRoundedIcon />
+        </div>
+        <div className="icon">
+          <StarRoundedIcon />
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Header;
