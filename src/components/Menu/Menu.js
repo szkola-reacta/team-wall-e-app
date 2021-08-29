daa75a871be81d958fee79806d12b96149354afe
Slawek <61154596+Ultran@users.noreply.github.com>
2021-08-30 00:45:53 +0200

    create nav items and add basic styles for future update

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,25 @@
+import SearchInput from "./SearchInput";
+
+import "./sass/app.scss";
+
 const Menu = () => {
-  return <p>menu</p>;
+  return (
+    <div className="sectionContainer">
+      <nav>
+        <ul className="list">
+          <li>menu</li>
+          <li>menu</li>
+          <li>menu</li>
+          <li>menu</li>
+          <li>menu</li>
+          <li>promotions</li>
+          <li>
+            <SearchInput />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Menu;
