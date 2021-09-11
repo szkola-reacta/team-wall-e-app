f2e3ac8455da4ece5575be2d591d2e2d3a71bab6
Mateusz Walter <wuoelte@gmail.com>
2021-09-11 13:55:49 +0200

    BestSales komponent

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
 import SearchInput from "./SearchInput";
-
+import BestSales from "../BestSales/BestSales";
 import "./sass/app.scss";
 
 const Menu = () => {
@@ -7,12 +7,12 @@ const Menu = () => {
     <div className="sectionContainer">
       <nav>
         <ul className="list">
+          <li><a href="Menu">menu</a></li>
           <li>menu</li>
           <li>menu</li>
           <li>menu</li>
           <li>menu</li>
-          <li>menu</li>
-          <li>promotions</li>
+          <li><a href="BestSales">promotions</a></li>
           <li>
             <SearchInput />
           </li>
