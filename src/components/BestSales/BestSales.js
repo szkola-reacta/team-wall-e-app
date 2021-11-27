383374df20e574b9983aa43d0e4b1a3aaf8a0656
mateu52 <wuoelte@gmail.com>
2021-11-27 20:57:21 +0100

    sales update demoView

--- src/components/BestSales/BestSales.js
+++ src/components/BestSales/BestSales.js
@@ -5,50 +5,157 @@ function BestSales(){
 
     return(
         <div className="SalesBack">
-            <div>
-                <div>
+            <div className="salesOffert">
+                <div className="salesHalf">
                     <img src="https://cdn.luxe.digital/media/2019/09/12085003/casual-dress-code-men-style-summer-luxe-digital.jpg"
                     alt="dress" width="100px" height="100px"></img>
                 </div>
-                <div>
-                    <h2>Biała koszula męska</h2>
-                    <h2>Levi's</h2>
-                </div>
-                <div>
-                    <h3>79 zł</h3>
-                    <h4>Kup teraz</h4>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Biała koszula męska</h2>
+                        <h2>Levi's</h2>
+                    </div>
+                    <div>
+                        <h3>79 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
                 </div>
             </div>
             
-            <div>
-                <div>
+            <div className="salesOffert">
+                <div className="salesHalf">
                     <img src="https://images.sport-shop.pl/big/1635338377/122935_1.jpg"
                     alt="dress" width="100px" height="100px"></img>
                 </div>
-                <div>
-                    <h2>Kurtka puchowa damska</h2>
-                    <h2>4F</h2>
-                </div>
-                <div>
-                    <h3>129 zł</h3>
-                    <h4>Kup teraz</h4>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Kurtka puchowa damska</h2>
+                        <h2>4F</h2>
+                    </div>
+                    <div>
+                        <h3>129 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <div>
+            <div className="salesOffert">
+                <div className="salesHalf">
                     <img src="https://images.sport-shop.pl/medium/1637578923/123846_1.jpg"
                     alt="dress" width="100px" height="100px"></img>
+                </div>
+                <div className="salesHalf">  
+                    <div>
+                        <h2>Bluza damska</h2>
+                        <h2>Puma</h2>
+                    </div>  
+                    <div>
+                        <h3>169 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
                 </div>  
-                <div>
-                    <h2>Bluza damska</h2>
-                    <h2>Puma</h2>
+            </div>
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://cdn.luxe.digital/media/2019/09/12085003/casual-dress-code-men-style-summer-luxe-digital.jpg"
+                    alt="dress" width="100px" height="100px"></img>
+                </div>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Biała koszula męska</h2>
+                        <h2>Levi's</h2>
+                    </div>
+                    <div>
+                        <h3>79 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://images.sport-shop.pl/big/1635338377/122935_1.jpg"
+                    alt="dress" width="100px" height="100px"></img>
+                </div>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Kurtka puchowa damska</h2>
+                        <h2>4F</h2>
+                    </div>
+                    <div>
+                        <h3>129 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://images.sport-shop.pl/medium/1637578923/123846_1.jpg"
+                    alt="dress"></img>
+                </div>
+                <div className="salesHalf">  
+                    <div>
+                        <h2>Bluza damska</h2>
+                        <h2>Puma</h2>
+                    </div>  
+                    <div>
+                        <h3>169 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
                 </div>  
-                <div>
-                    <h3>169 zł</h3>
-                    <h4>Kup teraz</h4>
+            </div>
+
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://cdn.luxe.digital/media/2019/09/12085003/casual-dress-code-men-style-summer-luxe-digital.jpg"
+                    alt="dress"></img>
+                </div>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Biała koszula męska</h2>
+                        <h2>Levi's</h2>
+                    </div>
+                    <div>
+                        <h3>79 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://images.sport-shop.pl/big/1635338377/122935_1.jpg"
+                    alt="dress"></img>
+                </div>
+                <div className="salesHalf">
+                    <div>
+                        <h2>Kurtka puchowa damska</h2>
+                        <h2>4F</h2>
+                    </div>
+                    <div>
+                        <h3>129 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="salesOffert">
+                <div className="salesHalf">
+                    <img src="https://images.sport-shop.pl/medium/1637578923/123846_1.jpg"
+                    alt="dress"></img>
+                </div>
+                <div className="salesHalf">  
+                    <div>
+                        <h2>Bluza damska</h2>
+                        <h2>Puma</h2>
+                    </div>  
+                    <div>
+                        <h3>169 zł</h3>
+                        <h4>Kup teraz</h4>
+                    </div>
                 </div>  
-            </div>                               
+            </div>                                       
         </div>
     )
 }
